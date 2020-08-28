e86d9eaa8e6ebb4507817fbfd4b6350ff69c5395
fatinabila49 <fatinabila49@gmail.com>
2020-08-28 22:45:24 +0800

    Updates

---
+++ precache-manifest.185b7be0a3bfc102d97d1df237821b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ea7cc9bb4bb86b726a00fb8225d696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "188ecfb09dfeeb4a7157",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "1a3bacb095b3c4394160",
+    "url": "/static/css/main.0b6409b8.chunk.css"
+  },
+  {
+    "revision": "188ecfb09dfeeb4a7157",
+    "url": "/static/js/2.dd564719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd564719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3bacb095b3c4394160",
+    "url": "/static/js/main.547cc0b6.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
